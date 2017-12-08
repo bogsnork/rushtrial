9d269cbda49d516227aabf7b39acf8ada860c87a
bogsnork <christoph.kratz@gmail.com>
2017-12-08 13:00:26 +0000

    completed merging script but issues with getting columns to merge.

--- scripts/rush_01.3_data_prep.R
+++ scripts/rush_01.3_data_prep.R
@@ -11,6 +11,7 @@ library(readxl)
 library(tidyverse)
 library(vegan)
 library(lubridate)
+library(VIM) #to investigate missing values
 
 #Import data-----
 
@@ -65,33 +66,38 @@ meadows_2015$X__1 <-
 
 pastures_2015$X__1 <- 
   recode_factor(pastures_2015$X__1,
+                "Loaction" = "Location",
                 "Calliergon cuspidatum" = "Calliergonella cuspidata",
                 "Ranuunculus  repens" = "Ranunculus repens", 
                 "Rhytidiadelphus squarosus" = "Rhytidiadelphus squarrosus",
                 "Rhytidiadelphus squaros" = "Rhytidiadelphus squarrosus")
 
-### remove blank lines and comments
-meadows_2016 <- meadows_2016[-c(7, 83, 84, 153), ]
-pastures_2016 <- pastures_2016[-c(7, 83, 84, 154, 155), ]
+  #species group lookup - so far only for 2015 species. Need to add remainder
+  
+  spp_groups <- rbind(meadows_2015[, c(1,2)], pastures_2015[, c(1,2)])
+  spp_groups <- unique(spp_groups)
+  colnames(spp_groups) <- c("species_name", "species_group")
+  spp_groups <- spp_groups[which(!spp_groups$species_name %in% 
+                                   c("Combined", "plot_ID", "Actual Quadrat Number", 
+                                     "Type", "Site", "Year", "Sample Ref", 
+                                     "Replicate number", "Replicate", "Treatment Plot", 
+                                     "Surveyor", "Date", "Location", "Loaction")), ]
+  
+  spp_groups$species_abbr <- make.cepnames(spp_groups$species_name)
+
+#remove taxon column
+meadows_2015$X__2 <- NULL
+pastures_2015$X__2 <- NULL
+
+
+### remove blank lines and comments 
+meadows_2016 <- meadows_2016[-c(7, 83, 84, 153:159), ]
+pastures_2016 <- pastures_2016[-c(7, 83, 84, 153), ]
 
 HP_2017 <- HP_2017[-c(6, 122, 123, 130:141),]
 HM_2017 <- HM_2017[-c(6, 15, 122, 123, 127:139),]
 LM_2017 <- LM_2017[-c(6, 15, 122:140),]
 VP_2017 <- VP_2017[-c(6, 122, 123, 129:140),]
-
-
-#species group lookup - so far only for 2015 species. Need to add remainder
-
-spp_groups <- rbind(meadows_2015[, c(1,2)], pastures_2015[, c(1,2)])
-spp_groups <- unique(spp_groups)
-colnames(spp_groups) <- c("species_name", "species_group")
-spp_groups <- spp_groups[which(!spp_groups$species_name %in% 
-                                 c("Combined", "plot_ID", "Actual Quadrat Number", 
-                                   "Type", "Site", "Year", "Sample Ref", 
-                                   "Replicate number", "Replicate", "Treatment Plot", 
-                                   "Surveyor", "Date", "Location", "Loaction")), ]
-
-spp_groups$species_abbr <- make.cepnames(spp_groups$species_name)
 
 # munge sheets
 rushdata_2015 <- full_join(meadows_2015, pastures_2015, by = "X__1")
@@ -102,19 +108,60 @@ rushdata_2017 <- full_join(HP_2017, HM_2017, by = "X__1")
 rushdata_2017 <- full_join(rushdata_2017, LM_2017, by = "X__1")
 rushdata_2017 <- full_join(rushdata_2017, VP_2017, by = "X__1")
 
+#Harmonise site-variable names
+
+rushdata_2016$X__1 <- 
+  recode_factor(rushdata_2016$X__1, "Loaction" = "Location")
+
+rushdata_2017$X__1 <- 
+  recode_factor(rushdata_2017$X__1,
+                "Quadrat (1-100)" = "Actual Quadrat Number", 
+                "Treatment Plot (1-8)" = "Treatment Plot", 
+                "Replicate (A/B/C)" = "Replicate number")
+
+rushdata_2017[2,] <- toupper(paste0(rushdata_2017[2,], rushdata_2017[3,]))
+
+rushdata_2017$X__1 <- as.character(rushdata_2017$X__1)
+rushdata_2017$X__1[2] <- "Location"
+
+
+#from 2015 remove the following: "Combined", "plot_ID", "Type", "Site", "Year", "Sample Ref", "Replicate", "Surveyor", "Date"
+
+rushdata_2015 <- 
+  rushdata_2015[which(!rushdata_2015$X__1 %in%
+                        c("Combined", "plot_ID", "Type", "Site", "Year", 
+                          "Sample Ref", "Replicate", "Surveyor", "Date")), ]
+
+#from 2016 and 2017 also remove:   "Bare Ground", "Plant Litter", "Total rush cover", "Total grass cover", "Total herb cover", "Total sedge cover", "Rush cover", "Grass cover", "Herb cover", "Sedge cover", "Bare ground", "Litter"
+
+rushdata_2016 <- 
+  rushdata_2016[which(!rushdata_2016$X__1 %in%
+                       c("Combined", "plot_ID", "Type", "Site", "Year", 
+                         "Sample Ref", "Replicate", "Surveyor", "Date",
+                         "Bare Ground", "Plant Litter", "Total rush cover", 
+                         "Total grass cover", "Total herb cover", 
+                         "Total sedge cover", "Rush cover", "Grass cover", 
+                         "Herb cover", "Sedge cover", "Bare ground", "Litter")),]
+
+
+rushdata_2017 <- rushdata_2017[which(!rushdata_2017$X__1 %in%
+                         c("Date", "Surveyor", "Rush cover", "Grass cover", 
+                           "Herb cover", "Sedge cover", "Bare ground", "Litter", 
+                           "Bryophyte cover")),]
+
+
 
 ### add year
-rushdata_2015 <- rbind(rushdata_2015[c(1:13),], 
-                       c("year", rep("2015", ncol(rushdata_2015)-1)), 
-                       rushdata_2015[c(14:nrow(rushdata_2015)),])
+rushdata_2015 <- rbind(c("year", rep("2015", ncol(rushdata_2015)-1)), 
+                       rushdata_2015)
 
-rushdata_2016 <- rbind(rushdata_2016[c(1:6),], 
-                       c("year", rep("2016", ncol(rushdata_2016)-1)), 
-                       rushdata_2016[c(7:nrow(rushdata_2016)),])
+rushdata_2016 <- rbind(c("year", rep("2016", ncol(rushdata_2016)-1)), 
+                       rushdata_2016)
 
-rushdata_2017 <- rbind(rushdata_2017[c(1:6),], 
-                       c("year", rep("2017", ncol(rushdata_2017)-1)), 
-                       rushdata_2017[c(7:nrow(rushdata_2017)),])
+rushdata_2017 <- rbind(c("year", rep("2017", ncol(rushdata_2017)-1)), 
+                       rushdata_2017)
+
+
 
 
 # munge years
@@ -122,16 +169,82 @@ rushdata <- full_join(rushdata_2015, rushdata_2016, by = "X__1")
 rushdata <- full_join(rushdata, rushdata_2017, by = "X__1")
 
 #check for duplicated names
-rushdata$X__1[duplicated(rushdata$X__1)] #Lathyrus pratensis
-rushdata$X__1[duplicated(rushdata$X__1)] <- "Lathyrus pratensis 2" #Lathyrus pratensis
+rushdata[which(rushdata$X__1 %in% 
+                 rushdata$X__1[duplicated(rushdata$X__1)]), 1] 
+
+#[1] "Calliergonella cuspidata"   "Calliergonella cuspidata"   "Calliergonella cuspidata" [4] "Rhytidiadelphus squarrosus" "Lathyrus pratensis"         NA    
+
+#not sure if I need to do anything about these at this stage, or why they are there.  
 
 
 #save colnames
 colnames_rush <- rushdata$X__1
 
 #transform
-rushdata <- data.frame(t(rushdata))
+rushdata_t <- data.frame(t(rushdata))
 
 #write col names
-names(rushdata) <- colnames_rush
-colnames(rushdata)
+names(rushdata_t) <- colnames_rush
+colnames(rushdata_t)
+
+#delete row 1
+rushdata_t <- rushdata_t[-1,]
+
+#add temporary rownames
+rownames(rushdata_t) <- seq(1:nrow(rushdata_t))
+
+
+#find duplicate col names
+names(rushdata_t)[which(names(rushdata_t) %in% 
+                 names(rushdata_t)[duplicated(names(rushdata_t))])] 
+
+#make them unique, and deal with later
+names(rushdata_t) <- make.names(names(rushdata_t), unique = TRUE)
+
+#reorganise
+startcols <- c("year", "Location", "Loaction", 
+               "Replicate.number", "Replicate..A.B.C.", 
+               "Treatment.Plot", "Treatment.Plot..1.8.", 
+               "Site..H.L.V.", "Type..M.P.",
+               "Actual.Quadrat.Number", "Quadrat..1.100.", 
+               "Total.mosses", "Bryophyte.cover", "NA.", "NA..1")
+
+rushdata_t <- select(rushdata_t, one_of(startcols),everything())
+
+#have a look where there are missing values
+VIM::matrixplot(rushdata_t[1:15], 
+                labels = substr(names(rushdata_t[1:15]), 1, 5))
+
+#have a look at the matrix plot, Location column doesn't seem to work, probably need to get some of the other cols from 2016 over.  Or maybe it was that loaction column
+
+
+temp <- rushdata_t 
+
+temp$Location[which(is.na(temp$Location))] <- 
+  temp$Loaction[which(is.na(temp$Location))]
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
